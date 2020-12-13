b5add07cf1f9c5307e7e09a5f1d74cdb313515c0
4lexRossi <devalexrossi@gmail.com>
2020-12-13 18:57:13 -0300

    Functional Paradigm example of use

---
+++ src/FatorialRecursivo/Class.java
@@ -0,0 +1,17 @@
+package FatorialRecursivo;
+
+import java.util.function.UnaryOperator;
+
+public class Class {
+	public static void main(String[] args) {
+		// Imperative Paradigm
+		int value = 10;
+		int result = value * 3;
+		System.out.println("The result is :: " + result);
+		
+		// Functional Paradigm
+		UnaryOperator<Integer> multiplicateValueTre = functionalValue -> functionalValue*3;
+		int functionalValue = 10;
+		System.out.println("The result is :: " + multiplicateValueTre.apply(10));
+	}
+}
