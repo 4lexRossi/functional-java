638fadac3be6a62fb36dfa28bbd038e647241b3b
4lexRossi <devalexrossi@gmail.com>
2020-12-15 21:05:03 -0300

    Fatorial recursive example

---
+++ src/FatorialRecursiveTailCall/FatorialRecursive.java
@@ -0,0 +1,15 @@
+package FatorialRecursiveTailCall;
+
+public class FatorialRecursive {
+	public static void main(String[] args) {
+		System.out.println(fatorial(5));
+	}
+	
+	public static int fatorial(int value) {
+		if(value == 1 ) {
+			return value;
+		} else {
+			return value * fatorial((value -1));
+		}
+	}
+}
